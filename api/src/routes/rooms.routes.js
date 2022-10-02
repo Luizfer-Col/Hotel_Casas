4ff57b4987c3c7fa0a2e99cf7756aa825a4fdfe1
AndresCasas19 <a.19.casas@gmail.com>
2022-10-02 14:22:00 -0500

    Rutas update and delete rooms

--- api/src/routes/rooms.routes.js
+++ api/src/routes/rooms.routes.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import { createRoom, getRooms } from "../controllers/rooms.controller.js";
-
+import {
+  getRooms,
+  getRoom,
+  getRoomsByType,
+  createRoom,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/rooms.controller.js";
 
 const router = Router();
 
-
 router.get("/rooms", getRooms);
+router.get("/rooms/:id", getRoom);
+// router.get("/rooms/type", getRoomsByType);
 router.post("/rooms", createRoom);
-
+router.put("/rooms/:id", updateRoom);
+router.delete("/rooms/:id", deleteRoom);
 
 export default router;
