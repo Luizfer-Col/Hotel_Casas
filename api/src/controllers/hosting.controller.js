305f3241d34a96438e9812c05fb87b0aae2e2290
Luizfer-Col <luizfercolorado@gmail.com>
2022-12-13 09:45:22 -0500

    final updates

--- api/src/controllers/hosting.controller.js
+++ api/src/controllers/hosting.controller.js
@@ -1,1 +1,20 @@
 import Hosting from "../models/Hosting.js";
+
+// not upddate
+
+export async function createHosting(req, res) {
+    try {
+        const { roomId, date, data } = req.body;
+        const newHosting = await Hosting.create({
+            roomId,
+            date,
+            data
+        });
+
+        res.json(newHosting);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+}
